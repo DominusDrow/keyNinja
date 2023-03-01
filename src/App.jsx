cfb8ce354d9878110728518388a969d749fc107b
DominusDrow <galacticovasquez@gmail.com>
2023-03-01 12:58:26 -0600

    start KeyNinja project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-gray-900 min-h-screen flex items-center justify-center">
+
+    </div>
+  )
+}
+
+export default App
